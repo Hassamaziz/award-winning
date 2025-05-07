6c8bd326259f2ed034a3b684df16794c6c14fdcd
hassamaziz <hassamaziz001@gmail.com>
2025-05-07 22:12:08 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="text-violet-300 text-5xl font-zentry">hello world</h1>
+    </main>
+  );
+};
+
+export default App;
