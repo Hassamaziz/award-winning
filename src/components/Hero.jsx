4416e170fe2b7beea19199438c66f8b6e276bc52
hassamaziz <hassamaziz001@gmail.com>
2025-05-08 21:27:06 +0500

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -80,6 +80,7 @@ const Hero = () => {
       </div>
       <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
           G<b>a</b>ming
+          
         </h1>
     </div>
   );
