f004b4a5ddaea7eef91e53f7df56a4b0aae809bd
hassamaziz <hassamaziz001@gmail.com>
2025-05-12 22:04:19 +0500

    .

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -40,14 +40,14 @@ const Hero = () => {
           width: "100%",
           height: "100%",
           duration: 1,
-          ease: "power1.inOut",
+          ease: "power2.inOut",
           onStart: () => nextVideoRef.current.play(),
         });
         gsap.from("#current-video", {
           transformOrigin: "center center",
           scale: 0,
           duration: 1.5,
-          ease: "power1.inOut",
+          ease: "power2.inOut",
         });
       }
     },
