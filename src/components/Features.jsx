96483ecc51797582b14162cd589828c0921bc6d4
hassamaziz <hassamaziz001@gmail.com>
2025-06-24 23:01:52 +0500

    working on features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,4 @@
-const BentoCard = ({ src, title, description}) => {
+const BentoCard = ({ src, title, description }) => {
   return (
     <div className="relative size-full">
       {" "}
@@ -10,11 +10,11 @@ const BentoCard = ({ src, title, description}) => {
         src={src}
       />
       <div className="relative z-10 flex size-full flex-col justify-between p-5 text-blue-50">
-        
         <div>
-          
           <h1 className="bento-title special-font">{title}</h1>
-          {description && ( <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>)}
+          {description && (
+            <p className="mt-3 max-w-64 text-xs md:text-base">{description}</p>
+          )}
         </div>
       </div>
     </div>
@@ -34,23 +34,54 @@ const Features = () => {
             agentic AI and blockchain lead the new economic paradigm.
           </p>
         </div>
-      
-      <div className="border-hsla mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh] ">
-        <BentoCard
-          src="videos/feature-1.mp4"
-          title={
-            <>
-              radia<b>n</b>t
-            </>
-          }
-          description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
-        />
-      </div>
-      <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
-      <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
-      <BentoCard src="videos/feature-2.mp4" />
-      </div>
-      </div>
+
+        <div className="border-hsla mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh] ">
+          <BentoCard
+            src="videos/feature-1.mp4"
+            title={
+              <>
+                radia<b>n</b>t
+              </>
+            }
+            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+          />
+        </div>
+        <div className="grid h-[135vh] grid-cols-2 grid-rows-3 gap-7">
+          <div className="bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2">
+            <BentoCard
+              src="videos/feature-2.mp4"
+              title={
+                <>
+                  zig<b>m</b>a
+                </>
+              }
+              description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+            />
+          </div>
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-3.mp4"
+              title={
+                <>
+                  n<b>e</b>xus
+                </>
+              }
+              description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+            />
+          </div>
+
+          <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+            <BentoCard
+              src="videos/feature-4.mp4"
+              title={
+                <>
+                  az<b>u</b>l
+                </>
+              }
+              description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+            />
+          </div>
+        </div>
       </div>
     </section>
   );
