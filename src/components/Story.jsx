496ef683f0b6b07e3ceb7bb8b6b3888a3729b901
hassamaziz <hassamaziz001@gmail.com>
2025-07-10 15:00:52 +0500

    .

---
+++ src/components/Story.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Story = () => {
+  return (
+    <section className='min-h-dvh w-screen bg-black text-blue-50' id='story'>
+    <div className='flex size-full flex-col items-center py-10 pb-24'>
+    <p></p>
+    </div>
+    </section>
+  )
+}
+
+export default Story
